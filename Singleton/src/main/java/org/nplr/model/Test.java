12064f9640fbeeb39f8dda8184f32f87b9ff62a7
Nelson <130504241+NelsonLacayo27@users.noreply.github.com>
2024-09-27 11:46:02 -0600

    Add files via upload

---
+++ Singleton/src/main/java/org/nplr/model/Test.java
@@ -0,0 +1,15 @@
+package org.nplr.model;
+
+public class Test {
+    private static Test instance = null;
+
+    private Test() {
+    }
+
+    public static Test getInstance() {
+        if (instance == null) {
+            instance = new Test();
+        }
+        return instance;
+    }
+}
