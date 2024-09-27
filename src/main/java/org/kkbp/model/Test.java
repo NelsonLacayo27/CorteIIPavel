caa8024b9722ae5a7ad49472f236a8e96a9ffc05
KendrickBarrios <kkbarrios@uamv.edu.ni>
2024-09-26 18:50:21 -0600

    Agregué mi clase Test

---
+++ src/main/java/org/kkbp/model/Test.java
@@ -0,0 +1,15 @@
+package org.kkbp.model;
+
+public class Test {
+    private static Test instance = null;
+
+    private Test() {
+    }
+
+    public static Test getInstance() {
+        if (instance == null) {
+            instance = new Test();
+        }
+        return instance;
+    }
+}
